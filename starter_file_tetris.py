7bc0be5d872d4d7cb4a4faf4ffaaff849fa182ba
CAG07 <craig.gehrig@gmail.com>
2022-11-29 21:45:47 -0600

    Check if position is valid

--- starter_file_tetris.py
+++ starter_file_tetris.py
@@ -169,10 +169,21 @@ def convert_shape_format(shape):
 
       for i, pos in enumerate(positions):
           positions[i] = (pos[0] - 2, pos[1] - 4)
-          #Time 38:43
+          
 
 def valid_space(shape, grid):
-	pass
+	accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+      accepted_pos = [j for sub in accepted_pos for j in sub]
+      #^This will take all the positions in our first accepted_pos list and flatten the list to eliminate sublists and make it easier to loop through
+
+      formatted = convert_shape_format(shape)
+
+      #This is to check if the shape position is valid
+      for pos in formatted:
+            if pos not in accepted_pos:
+                  if pos[1] > -1:
+                        return False
+      return True
 
 def check_lost(positions):
 	pass
